3ed20262f6548bc6bf3a33606fb66133b8a57162
zkan <zkan.cs@gmail.com>
2021-01-31 00:13:14 +0700

    Test Arrow with Parquet and work around with ARROW_PRE_0_15_IPC_FORMAT when enable for conversion to/from Pandas :bear:

--- playground.py
+++ playground.py
@@ -2,6 +2,7 @@ import time
 
 import pandas as pd
 import pyarrow.csv as pc
+import pyarrow.parquet as pq
 from pyspark.sql import SparkSession
 
 
@@ -12,7 +13,7 @@ tic = time.perf_counter()
 pandas_df = pd.read_csv(file_name)
 toc = time.perf_counter()
 print(type(pandas_df))
-print(f'Pandas read in {toc - tic:0.4f} seconds')
+print(f'Pandas read (CSV) in {toc - tic:0.4f} seconds')
 
 # PySpark
 spark = SparkSession.builder.master('local[1]').appName('MyApp').getOrCreate()
@@ -21,7 +22,7 @@ tic = time.perf_counter()
 spark_df = spark.read.csv(file_name)
 toc = time.perf_counter()
 print(type(spark_df))
-print(f'PySpark read in {toc - tic:0.4f} seconds')
+print(f'PySpark read (CSV) in {toc - tic:0.4f} seconds')
 
 # Arrow
 tic = time.perf_counter()
@@ -29,21 +30,35 @@ table = pc.read_csv(file_name)
 df = table.to_pandas()
 toc = time.perf_counter()
 print(type(df))
-print(f'Arrow read (to Pandas) in {toc - tic:0.4f} seconds')
+print(f'Arrow read (CSV to Pandas) in {toc - tic:0.4f} seconds')
+
+pq.write_table(table, 'building_permits.parquet')
+
+tic = time.perf_counter()
+pdf = pd.read_parquet('building_permits.parquet')
+toc = time.perf_counter()
+print(f'Pandas read (Parquet) in {toc - tic:0.4f} seconds')
+
+tic = time.perf_counter()
+table = pq.read_table('building_permits.parquet')
+toc = time.perf_counter()
+print(f'Arrow read (Parquet) in {toc - tic:0.4f} seconds')
+
 
 # ----------
 
-# tic = time.perf_counter()
-# pdf = spark_df.toPandas()
-# toc = time.perf_counter()
-# print(type(pdf))
-# print(f'Spark DF to Pandas without Arrow in {toc - tic:0.4f} seconds')
+tic = time.perf_counter()
+pdf = spark_df.toPandas()
+toc = time.perf_counter()
+print(type(pdf))
+print(f'Spark DF to Pandas without Arrow in {toc - tic:0.4f} seconds')
 
 # Got memory leak issue
-# spark.conf.set('spark.sql.execution.arrow.pyspark.enabled', 'true')
-#
-# tic = time.perf_counter()
-# pdf = spark_df.toPandas()
-# toc = time.perf_counter()
-# print(type(pdf))
-# print(f'Spark DF to Pandas using Arrow in {toc - tic:0.4f} seconds')
+# Set env `export ARROW_PRE_0_15_IPC_FORMAT=1` (Credit: https://george-jen.gitbook.io/data-science-and-apache-spark/enabling-for-conversion-to-from-pandas)
+spark.conf.set('spark.sql.execution.arrow.pyspark.enabled', 'true')
+
+tic = time.perf_counter()
+pdf = spark_df.toPandas()
+toc = time.perf_counter()
+print(type(pdf))
+print(f'Spark DF to Pandas using Arrow in {toc - tic:0.4f} seconds')
